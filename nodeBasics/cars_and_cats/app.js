2020046f511bb5fd8dcfb2958496712cd47900b9
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-02-21 09:45:58 -0800

    added new.html and routes to cars_and_cats

--- nodeBasics/cars_and_cats/app.js
+++ nodeBasics/cars_and_cats/app.js
@@ -17,6 +17,14 @@ var server = http.createServer(function (request, response){
             response.end(); // finished!
         });
     }
+    else if(request.url === '/cars/new') {
+        fs.readFile("views/new.html", 'utf8', function (errors, contents){
+            
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
     else if(request.url === '/cars') {
         fs.readFile("views/cars.html", 'utf8', function (errors, contents){
             

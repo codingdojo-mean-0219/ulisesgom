2f1e8d7769ee9252ee0e2d244558aa4a01ab3fe1
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-03-04 11:53:11 -0800

    modified server code and made it DRY, same for jquery code

--- express/swapi/server.js
+++ express/swapi/server.js
@@ -21,15 +21,33 @@ app.get('/', (req, res) => {
 });
 
 app.get('/people', (req,res) => {
-    axios.get('https://swapi.co/api/people').then(response => {
-        console.log(response.data);
-        res.json(response.data);
-    }).catch(err => {
-        console.log(response.err);
-        res.json(response.err);
-    });
-})
+    req.session.nextPage = 'https://swapi.co/api/people';
+    axiosSwapi(req,res,req.session.nextPage);
+});
 
+app.get('/planets', (req,res) => {
+    req.session.nextPage = 'https://swapi.co/api/planets';
+    console.log('here')
+    axiosSwapi(req,res,req.session.nextPage);
+});
+
+app.get('/next', (req,res) => {
+    axiosSwapi(req,res,req.session.nextPage);
+});
+app.get('/prev', (req,res) => {
+    axiosSwapi(req,res,req.session.prevPage);
+});
+
+function axiosSwapi(request, response, url) {
+    axios.get(url).then(data => {
+        request.session.nextPage = data.data.next;
+        request.session.prevPage = data.data.previous;
+        response.json(data.data);
+    }).catch(err => {
+        console.log(data.err);
+        response.json(data.err);
+    });
+}
 
 app.listen(8000, function() {
     console.log("on 8000")

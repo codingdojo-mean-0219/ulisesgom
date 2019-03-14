0d996a570865ac2645bc68cb7731863be0fd59c3
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-03-14 12:07:13 -0700

    added more modularization, controllers, to quoting dojo

--- MongoDB/quoting_dojo/server/config/routes.js
+++ MongoDB/quoting_dojo/server/config/routes.js
@@ -1,44 +1,14 @@
 const mongoose = require('mongoose');
 const Quote = mongoose.model('Quote');
-
+const quotes = require('../controllers/quotes')
 module.exports = function(app) {
     app.get('/', function(req, res) {
-    
-        res.render('index');    
+        quotes.index(req,res);
     });
     app.post('/quotes', function(req, res) {
-        console.log("POST DATA", req.body);
-        var quote = new Quote(req.body);
-        // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-        quote.save(function(err) {
-          // if there is an error console.log that something went wrong!
-            if(err) {
-                console.log("We have an error!", err);
-                // adjust the code below as needed to create a flash message with the tag and content you would like
-                for(var key in err.errors){
-                    req.flash('registration', err.errors[key].message);
-                }
-                // redirect the user to an appropriate route
-                res.redirect('/')
-            } else { // else console.log that we did well and then redirect to the root route
-                console.log('successfully added a user!');
-                res.redirect('/quotes');
-            };
-        })
+        quotes.new(req,res);
     });
-    
     app.get('/quotes',(req,res) => {
-        let allQuotes;
-        Quote.find({}, null, {sort: '-createdAt'}, (err, quotes) => {
-            if(err) {
-                console.log(err);
-                redirect('/');
-            } else {
-                console.log(quotes, "in quotes find()");
-                allQuotes = quotes;
-                res.render('quotes', {quotes: allQuotes});
-            }
-        })
-        
-    })
-}
+        quotes.show(req,res);
+    });
+};
---
+++ MongoDB/quoting_dojo/server/controllers/quotes.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Quote = mongoose.model('Quote');
+
+module.exports = {
+    index: function(req,res) {
+        res.render('index');
+    },
+    new: function(req,res) {
+        console.log("POST DATA", req.body);
+        var quote = new Quote(req.body);
+        // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+        quote.save(function(err) {
+          // if there is an error console.log that something went wrong!
+            if(err) {
+                console.log("We have an error!", err);
+                // adjust the code below as needed to create a flash message with the tag and content you would like
+                for(var key in err.errors){
+                    req.flash('registration', err.errors[key].message);
+                }
+                // redirect the user to an appropriate route
+                res.redirect('/')
+            } else { // else console.log that we did well and then redirect to the root route
+                console.log('successfully added a user!');
+                res.redirect('/quotes');
+            };
+        });
+    },
+    show: function(req,res) {
+        Quote.find({}, null, {sort: '-createdAt'}, (err, quotes) => {
+            if(err) {
+                console.log(err);
+                redirect('/');
+            } else {
+                console.log(quotes, "in quotes find()");
+                allQuotes = quotes;
+                res.render('quotes', {quotes});
+            }
+        })
+    }
+
+};

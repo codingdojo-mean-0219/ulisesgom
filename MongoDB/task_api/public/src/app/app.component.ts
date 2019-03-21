2a95e7c05fceef83b7709a8b9b46681e99b190b0
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-03-21 12:14:13 -0700

    added dom manipulation for taskapi

--- MongoDB/task_api/public/src/app/app.component.ts
+++ MongoDB/task_api/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,29 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MEAN';
+export class AppComponent implements OnInit {
+  allTasks;
+  oneTask;
+  
   constructor(private _httpService: HttpService){}
+
+  ngOnInit() {
+    this.getTasksFromService();
+    this.getOneTaskFromService();
+  }
+  getTasksFromService() {
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log('all', data);
+      this.allTasks = data['docs'];
+    })
+  };
+  getOneTaskFromService() {
+    let observable = this._httpService.getTaskById('5c8d62017a8b9c2b8c762c08');
+    observable.subscribe(data => {
+      console.log('single', data);
+      this.oneTask = data['doc'];
+    })
+  }
 }
+

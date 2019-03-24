f885d3b5a4d79a249c5955818677557c54c2b690
ulisesgomez120 <ulisesgomez120@gmail.com>
2019-03-24 14:59:47 -0700

    added nested component rateMyCakes

--- angular/rateMyCakes/public/src/app/cake/cake.component.ts
+++ angular/rateMyCakes/public/src/app/cake/cake.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cake',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cake.component.css']
 })
 export class CakeComponent implements OnInit {
-
+  @Input() showCake: any;
   constructor() { }
 
   ngOnInit() {
